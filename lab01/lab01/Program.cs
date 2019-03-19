d2282d300756a68f7e9610e058965b6d803005d2
zhengwjie <495036225@qq.com>
2019-03-19 15:52:54 +0800

    555

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -10,64 +10,128 @@ using System.Drawing;
 using System.Drawing.Imaging;
 namespace lab01
 {
-    
+
 
     class Program
     {
 
         static void Main(String[] args)
         {
-            int k=0;
-            //摘要:
-            //    method判断将二维码输出到控制台还是保存在文件中
-            bool method = false;
-            for(int i=0;i<args.Length;i++)
+            try
             {
-                if (args[i] == "-f")
-                {
-                    k = i + 1;
-                    method = true;
-                }
-            }
-            //摘要：
-            //     判断输入的文件是Excel,txt
-            //     
-            if(method&&k<args.Length)
-            {
-                String[] str = args[k].Split('.');
-                String last = str[str.Length - 1];
-                if (last == "txt")
-                    TxtToQrcode.TxtToPNG(args[k]);
-                else if (last == "xlsx")
-                {
-                    TxtToQrcode.ExcelToJsonToQrcode(args[k]);
-                    TxtToQrcode.ExcelToXMLToQrcode(args[k]);
-                }
-                else if(args[k]=="mysql")
-                {
-                    TxtToQrcode.MysqlToxmlQrcode("server=localhost;Initial Catalog=chap1;User ID=root;Password=123456",
-                "select * from student;select * from students");
-                    TxtToQrcode.MysqlToJsonQrcode("server=localhost;Initial Catalog=chap1;User ID=root;Password=123456",
-                "select * from students;select * from student");
-                }
-                else
+                if (args.Length == 0)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("不支持的文件格式！！！");
+                    Console.WriteLine("请传入相关参数");
                     Console.ResetColor();
+                    return;
+                }
+                int k = 0;
+                //摘要:
+                //    method判断将二维码输出到控制台还是保存在文件中
+                bool method = false;
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] == "-f")
+                    {
+                        k = i + 1;
+                        method = true;
+                        if (args.Length <= k)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("请输入文件名！");
+                            Console.ResetColor();
+                            return;
+                        }
+                    }
+                }
+                //摘要：
+                //     判断输入的文件是Excel,txt
+                //     
+                if (method)
+                {
+                    String[] str = args[k].Split('.');
+                    String last = str[str.Length - 1];
+                    if (last == "txt")
+                    {
+                        try
+                        {
+                            TxtToQrcode.TxtToPNG(args[k]);
+                        }catch(Exception)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("找不到文件！");
+                            Console.ResetColor();
+                            return;
+                        }
+                    } 
+                    else if (last == "xlsx")
+                    {
+                        try
+                        {
+                            TxtToQrcode.ExcelToJsonToQrcode(args[k]);
+                            TxtToQrcode.ExcelToXMLToQrcode(args[k]);
+                        }
+                        catch (Exception)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("找不到文件！");
+                            Console.ResetColor();
+                            return;
+                        }
+                    }
+                    else if (args[k] == "mysql")
+                    {
+                        k = k + 2;
+                        if (k >= args.Length)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("请输入连接字符串与查询语句！");
+                            Console.ResetColor();
+                            return;
+                        }
+                        try
+                        {
+                            TxtToQrcode.MysqlToxmlQrcode(args[k - 1], args[k]);
+                            TxtToQrcode.MysqlToJsonQrcode(args[k - 1], args[k]);
+                        }
+                        catch (Exception)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("连接字符串或查询语句错误！");
+                            Console.ResetColor();
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("不支持的文件格式！！！");
+                        Console.ResetColor();
+                        return;
+                    }
+                }
+                //摘要:
+                //    输出到控制台中
+                //    将第二个命令行参数作为要生成二维码的信息
+                else
+                {
+                    try
+                    {
+                        TxtToQrcode.StringToConsole(args[1]);
+                    } catch (Exception)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("请输入要生成二维码的字符串");
+                        Console.ResetColor();
+                        return;
+                    }
                 }
             }
-            //摘要:
-            //    输出到控制台中
-            else
+            catch (Exception)
             {
-                string str = "";
-                for (int i = 0; i < args.Length - 1; i++)
-                    str += args[i] + " ";
-                str += args[args.Length - 1];
-                TxtToQrcode.StringToConsole(str);
+                Console.WriteLine("参数错误");
             }
         }
-
     }
 }
--- lab01/lab01/TxtToQrcode.cs
+++ lab01/lab01/TxtToQrcode.cs
@@ -20,14 +20,8 @@ namespace lab01
         //      将str字符串转化成QRCode,以png文件格式存储
         public static void TxtToPNG(string path)
         {
-            if (!File.Exists(path))
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("文件不存在！");
-                Console.ResetColor();
-                return;
-            }
             StreamReader reader = new StreamReader(path);
+            string dir=null;
             int line = 1;
             // 摘要：
             //      读入一行，将生成的二维码存储在bmp文件中
@@ -47,41 +41,41 @@ namespace lab01
                 {
                     //摘要：
                     //    保存文件路径
-                    string dir = path.Substring(0, path.LastIndexOf('\\')) + @"\txt\";
+                    dir = path.Substring(0, path.LastIndexOf('\\')) + @"\txt";
                     if (!Directory.Exists(dir))
                     {
                         Directory.CreateDirectory(dir);
                     }
-                    String fileName = dir + line.ToString("000") + str.Substring(0, 4) + ".png";
+                    String fileName = dir +"\\" + line.ToString("000") + str.Substring(0, 4) + ".png";
                     TxtToQrcode.StringToPng(str, fileName);
                 }
                 line++;
             }
-            Console.WriteLine("已完成将txt文本信息转换为Qrcode并存入F:\\c#\\lab01\\txt文件夹");
+            Console.WriteLine("已完成将txt文本信息转换为Qrcode并存入{0}文件夹",dir);
         }
             //摘要:
             //    将字符串生成二维码,并输出到控制台
         public static void StringToConsole(String str)
-            {
-                //摘要：
-                //    判断字符串是否符合要求
-                if (str.Length >= 25)
+        {
+            //摘要：
+            //    判断字符串是否符合要求
+            if (str.Length >= 25)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("命令行参数太多！");
                     Console.ResetColor();
                     return;
                 }
-                //摘要：
-                //    生成二维码
-                QrEncoder qre = new QrEncoder(ErrorCorrectionLevel.M);
-                QrCode qrc = qre.Encode(str);
-                //摘要：
-                //    将二维码输出到控制台
-                for (int i = 0; i < qrc.Matrix.Height; i++)
+            //摘要：
+            //    生成二维码
+            QrEncoder qre = new QrEncoder(ErrorCorrectionLevel.M);
+            QrCode qrc = qre.Encode(str);
+            //摘要：
+            //    将二维码输出到控制台
+            for (int i = 0; i < qrc.Matrix.Height; i++)
+            {
+                if (i == 0)
                 {
-                    if (i == 0)
-                    {
                         for (int j = 0; j < (qrc.Matrix.Width + 2); j++)
                         {
                             Console.BackgroundColor = ConsoleColor.White;
@@ -162,14 +156,14 @@ namespace lab01
             //摘要：
             //    在Excel文件所在目录下，创建excelToPng.png的文件
             //    存储Qrcode
-            String dir = path.Substring(0, path.LastIndexOf('\\'))+@"\excel\";
+            String dir = path.Substring(0, path.LastIndexOf('\\'))+@"\excel";
             if(!Directory.Exists(dir))
             {
                 Directory.CreateDirectory(dir);
             }
             String filename =dir +@"\xml.png";
             TxtToQrcode.StringToPng(str, filename);
-            Console.WriteLine("已完成将Excel信息转换为Xml字符串并存入F:\\c#\\lab01\\excel文件夹");
+            Console.WriteLine("已完成将Excel信息转换为Xml字符串并存入{0}文件夹",dir);
         }
         /// <summary>
         /// 将字符串变成QrCode，以png格式存储在文件中
@@ -266,12 +260,12 @@ namespace lab01
             //摘要：
             //    将DataTable数据转换成Json格式字符串
             String s =TxtToQrcode.DataTableToJson(dt);
-            String dir = path.Substring(0, path.LastIndexOf('\\'))+"\\excel\\";
+            String dir = path.Substring(0, path.LastIndexOf('\\'))+"\\excel";
             if (!Directory.Exists(dir))
                 Directory.CreateDirectory(dir);
             String fileName = dir + @"\json.png";
             TxtToQrcode.StringToPng(s, fileName);
-            Console.WriteLine("已完成将Excel文件信息转换为Json字符串并存入F:\\c#\\lab01\\excel文件夹");
+            Console.WriteLine("已完成将Excel文件信息转换为Json字符串并存入{0}文件夹",dir);
         }
         /// <summary>
         /// 将DataTable数据转换成Json格式字符串
@@ -305,7 +299,7 @@ namespace lab01
 
         /// <summary>
         /// 对Mysql数据库进行操作
-        /// 生成以Xml格式的字符串，并生成二维码，存储在F:/mysql文件夹下的xml.png中
+        /// 生成以Xml格式的字符串，并生成二维码，存储在mysql文件夹下的xml.png中
         /// </summary>
         /// <param name="str">连接数据库所需字符串</param>
         /// <param name="command">对数据库操作的命令</param>
@@ -330,18 +324,19 @@ namespace lab01
             s1.Append(xml[xml.Length - 1]);
             s1.Append('\n');
             xml = s1.ToString();
-            String fileName = "F:\\c#\\lab01\\mysql";
+            String fileName = "mysql";
             if(!Directory.Exists(fileName))
             {
                 Directory.CreateDirectory(fileName);
             }
             TxtToQrcode.StringToPng(xml, fileName + "\\xml.png");
-            Console.WriteLine("已完成将Mysql数据库查询信息转换为Xml字符串并存入F:\\c#\\lab01\\mysql文件夹");
+            Console.WriteLine("已完成将Mysql数据库查询信息转换为Xml字符串并存入{0}文件夹",
+                System.Environment.CurrentDirectory+"\\"+fileName);
         }
 
         /// <summary>
         /// 对Mysql数据库进行操作
-        /// 生成以Json格式的字符串，并生成二维码，存储在F:/mysql文件夹下的json.png中
+        /// 生成以Json格式的字符串，并生成二维码，存储在mysql文件夹下的json.png中
         /// </summary>
         /// <param name="str">连接数据库所需字符串</param>
         /// <param name="command">对数据库操作的命令</param>
@@ -360,17 +355,17 @@ namespace lab01
                 {
                     System.Data.DataTable st = ds.Tables[i];
                     String str1 = TxtToQrcode.DataTableToJson(st);
-                    if(!Directory.Exists("F:\\c#\\lab01\\mysql"))
+                    if(!Directory.Exists("mysql"))
                     {
-                        Directory.CreateDirectory("F:\\c#\\lab01\\mysql");
+                        Directory.CreateDirectory("mysql");
                     }
-                    String s = string.Format("F:\\c#\\lab01\\mysql\\json{0}.png", i);
+                    String s = string.Format("mysql\\json{0}.png", i);
                     TxtToQrcode.StringToPng(str1, s);
-                    
                     i++;
                 }
             }
-            Console.WriteLine("已完成将Mysql数据库查询信息转换为Json字符串并存入F:\\c#\\lab01\\mysql文件夹");
+            Console.WriteLine("已完成将Mysql数据库查询信息转换为Json字符串并存入{0}文件夹",
+                System.Environment.CurrentDirectory + "\\mysql");
         }
     }
 
